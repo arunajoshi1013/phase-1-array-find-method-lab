5a0286c24dd27a1c4550713bda78d3cd960d5d87
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-03-31 22:51:27 -0700

    complete array find lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function isAWin(element, index, array) {
+   return (element.result === "W")
+}
+
+function superbowlWin(record) {
+   const found = record.find(isAWin)
+   if (found) {
+      return found.year
+   }
+   return found
+}
